793dd69f75db7cff64df9571f8f9f85565d18540
VictoriaSzy <vszymanowski00@stuy.edu>
2019-03-11 11:46:04 -0400

    Took absolute value of pivot and corrected arrayoutofboundsexception

--- Quick.java
+++ Quick.java
@@ -2,6 +2,25 @@ import java.util.* ;
 import java.io.* ;
 
 public class Quick {
+  public static void main(String[] args) {
+    int[]ary = { 2, 10, 15, 23, 0,  5} ;  //sorted :  {0,2,5,10,15,23}
+    quickselect(ary , 0 ) ; // would return 0
+    quickselect(ary , 1 ) ; // would return 2
+    quickselect(ary , 2 ) ; // would return 5
+    quickselect(ary , 3 ) ; // would return 10
+    quickselect(ary , 4 ) ; // would return 15
+    quickselect(ary , 5 ) ; // would return 23
+  }
+
+  /*return the value that is the kth smallest value of the array.
+  */
+  public static int quickselect(int[] data, int k) {
+    System.out.println("We are beginning quickselect now!") ;
+    if (k < 0 || k >= data.length) {
+      throw new IllegalArgumentException("k is not a valid argument!") ;
+    }
+    return partition(data, 0, data.length - 1) ;
+  }
   /*Modify the array such that:
   *1. Only the indices from start to end inclusive are considered in range
   *2. A random index from start to end inclusive is chosen, the corresponding
@@ -12,14 +31,16 @@ public class Quick {
   */
   public static int partition(int [] data, int start, int end) {
     int lim = end - start + 1 ;
+    System.out.println("The limit is " + lim) ;
     Random a = new Random() ;
-    int pivot = a.nextInt() % lim ;
+    int pivot = Math.abs(a.nextInt() % lim) ;
     pivot += start ;
-    //System.out.println("The pivot is: " + pivot) ;
+    System.out.println("The pivot is: " + pivot) ;
     int temp = data[start] ;
     data[start] = data[pivot] ;
     data[pivot] = temp ;
     int s = start ;
+    System.out.println("We are beginning to compare values now!") ;
     for (int i = start + 1 ; i < end + 1 ; i++) {
       if (data[i] < data[start]) {
         // it goes to the left!
@@ -27,6 +48,10 @@ public class Quick {
         temp = data[s] ;
         data[s] = data[i] ;
         data[i] = temp ;
+      }
+      if (data[i] > data[start]) {
+        // it goes to the right
+
       }
     }
     temp = data[start] ;
